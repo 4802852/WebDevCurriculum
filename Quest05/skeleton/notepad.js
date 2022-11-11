ad29fda5ef1fc048ca61107473101eb7e5cb7e80
4802852 <4802852@gmail.com>
2022-11-12 02:11:52 +0900

    modify: quest05

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -1,3 +1,96 @@
 class Notepad {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-};
+  constructor(name) {
+    this.name = name;
+    this.text = "";
+    Notepad.createListNode(name);
+    Notepad.createTabNode(name);
+  }
+
+  static createListNode(name) {
+    const li = document.createElement("li");
+    li.className = "noteList";
+    li.textContent = name;
+    Control.notes.appendChild(li);
+  }
+
+  static createTabNode(name) {
+    const li = document.createElement("li");
+    li.className = "tabList";
+    li.textContent = name;
+    Control.tabs.appendChild(li);
+  }
+}
+
+class Control {
+  static menus = document.getElementById("fileMenu");
+  static notes = document.getElementById("fileList");
+  static tabs = document.getElementById("tabList");
+  static textBox = document.getElementById("textBox");
+  static newBtn = document.getElementById("newBtn");
+  static loadBtn = document.getElementById("loadBtn");
+  static saveBtn = document.getElementById("saveBtn");
+  static saveAsBtn = document.getElementById("saveAsBtn");
+
+  static activeTab;
+  static priorActiveTab;
+
+  static initNote() {
+    for (let i = 0; i < localStorage.length; i++) {
+      Notepad.createListNode(localStorage.key(i));
+    }
+  }
+
+  static selectTab(e) {
+    if (Control.activeTab) {
+    }
+  }
+
+  static createNewFile() {
+    let name = prompt("파일명을 입력해주세요");
+
+    let nl = this.notes.getElementsByTagName("li");
+    for (let i = 0; i < nl.length; i++) {
+      if (nl[i].innerText === name) {
+        alert("동일한 이름을 가진 파일이 있습니다");
+        return;
+      }
+    }
+
+    let notepad = new Notepad(name);
+    localStorage.setItem(name, JSON.stringify(notepad));
+  }
+
+  static saveText() {
+    let file = JSON.parse(localStorage.getItem(this.activeTab));
+    file.text = Control.textBox.value;
+    file.tmpText = null;
+    localStorage.setItem(file.name, JSON.stringify(file));
+  }
+
+  static tmpText() {
+    let file = JSON.parse(localStorage.getItem(this.activeTab));
+    file.tmpText = Control.textBox.value;
+    localStorage.setItem(file.name, JSON.stringify(file));
+  }
+
+  static activateTab(name) {
+    let tl = this.tabs.getElementsByTagName("li");
+    for (let i = 0; i < tl.length; i++) {
+      if (tl[i].innerText === name) {
+        tl[i].className += "activeTab";
+      }
+    }
+  }
+}
+
+function menuClickHandler(e) {
+  switch (e.target.id) {
+    case "newBtn":
+      Control.createNewFile();
+      break;
+  }
+}
+
+Control.initNote();
+
+Control.menus.addEventListener("click", menuClickHandler);
