98c54fa4af73b57c65a8c2272f29966191285b75
4802852 <4802852@gmail.com>
2022-11-20 16:29:21 +0900

    add: Quest07.md

--- Quest07/skeleton/esm-my-project/index.mjs
+++ Quest07/skeleton/esm-my-project/index.mjs
@@ -1,1 +1,3 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+
+import { EsmUtilClass, esmUtilFunction } from "../esm-package/index.mjs";
--- Quest07/skeleton/esm-package/index.mjs
+++ Quest07/skeleton/esm-package/index.mjs
@@ -1,15 +1,17 @@
 class EsmUtilClass {
-    constructor(foo) {
-        this.foo = foo;
-    }
+  constructor(foo) {
+    this.foo = foo;
+  }
 
-    double() {
-        return foo * 2;
-    }
+  double() {
+    return foo * 2;
+  }
 }
 
-const esmUtilFunction = str => {
-    return str.toUpperCase();
+const esmUtilFunction = (str) => {
+  return str.toUpperCase();
 };
 
 // TODO: 다른 패키지가 EsmUtilClass와 esmUtilFunction를 가져다 쓰려면 어떻게 해야 할까요?
+
+export { EsmUtilClass, esmUtilFunction };
